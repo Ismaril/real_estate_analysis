b908705233286002ecff8af86f34836a96c67d4a
tomlazen <oismarilo@gmail.com>
2022-08-24 22:06:41 +0200

    first commit

---
+++ constants.py
@@ -0,0 +1,50 @@
+# names of columns in csv
+LINK = "Link:"
+LOC = "loc"
+LOCATION = "Lokace:"
+UNNAMED = "Unnamed: 0"
+LASTMOD = "lastmod"
+DATETIME_SCRAPED = "Řádek vytvořen:"
+TOTAL_PRICE = "Celková cena:"
+ON_SALE = "Zlevněno:"
+PROPERTY = "Nemovitost:"
+TYPE = "Typ:"
+STRUCTURE = "Stavba:"
+STATE_OF_OBJECT = "Stav objektu:"
+OWNERSHIP = "Vlastnictví:"
+FORMER_PRICE = "Původní cena:"
+BUILD_UP_AREA = "Plocha zastavěná:"
+USABLE_AREA = "Užitná plocha:"
+LAND_AREA = "Plocha pozemku:"
+GARDEN_AREA = "Plocha zahrady:"
+
+# strings
+COMMERCIAL = "komercni"
+OTHERS = "ostatni"
+RENT = "pronajem"
+AUCTION = "drazby"
+SITEMAP = "sitemap"
+
+# links to websites (csv)
+PROPERTIES_LINKS = "links_to_properties/links_to_properties.csv"
+PROPERTIES_CLEANED_LINKS = "links_to_properties/links_to_properties_cleaned.csv"
+
+# downloaded features of each property (csv)
+FEATURES = "features/features_all.csv"
+
+# user agents
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+    "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+]
+
+# address to chrome driver (exe)
+DRIVER_ADDRESS = "chrome_driver/chromedriver.exe"
+
+# sitemap (xml)
+SITE_MAP = "https://www.sreality.cz/sitemap.xml"
+
+# local directories
+DOWNLOADS = "C:/Users/lazni/Downloads"
