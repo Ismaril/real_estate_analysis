4ee3aab6f80468f6ed980e77cc6673a2d6ab4730
tomlazen <oismarilo@gmail.com>
2022-09-05 22:11:19 +0200

    added next cleaning functs and plotting

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import scraper
 import data_cleaning
-import constants as c
+import scraper
+import plotting
 
 
 def main(scrape_sitemaps=True,
          scrape_features=True,
-         clean_features=True):
+         clean_features=True,
+         plot_data=True):
 
     if scrape_sitemaps:
         scraper.site_map_scraping(scraper.chrome_driver(headless=False))
@@ -13,16 +14,23 @@ def main(scrape_sitemaps=True,
         data_cleaning.clean_raw_data()
 
     if scrape_features:
-        scraper.properties_scraping(driver=scraper.chrome_driver(headless=True),
-                                    dataset=c.PROPERTIES_CLEANED_LINKS,
-                                    batch_size=1_000,
-                                    max_nr_samples=None)
+        scraper.features_scraping(driver=scraper.chrome_driver(headless=True),
+                                  batch_size=1_000,
+                                  max_nr_samples=None)
 
     if clean_features:
         data_cleaning.concatenate_batches()
+        data_cleaning.clean_feature_data()
+        data_cleaning.aggregate_feature_data()
+        data_cleaning.archive_datasets()
+        data_cleaning.delete_all_batches()
+
+    if plot_data:
+        plotting.plot_all()
 
 
 if __name__ == '__main__':
-    main(scrape_sitemaps=False,
-         scrape_features=True,
-         clean_features=False)
+    main(scrape_sitemaps=True,
+         scrape_features=False,
+         clean_features=False,
+         plot_data=False)
