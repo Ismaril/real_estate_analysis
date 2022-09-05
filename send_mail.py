e157ca0ea921c75304e88b66e13d37c9dfab6c40
tomlazen <oismarilo@gmail.com>
2022-09-05 21:59:03 +0200

    login separated from project

--- send_mail.py
+++ send_mail.py
@@ -1,19 +1,27 @@
 import smtplib
+import constants as c
 
-ADDRESS = "oismarilo@gmail.com"
-PASS = [0.12, 0.119, 0.099, 0.122, 0.1, 0.104, 0.099, 0.108, 0.104, 0.118, 0.114, 0.112, 0.113, 0.1, 0.102, 0.108]
+with open(f"{c.DOWNLOADS}/robot_conect.txt", "r") as file:
+    file_complete = file.read().split("\n")
+    ADDRESS = file_complete[0]
+    PASS = file_complete[1]
 
 
-def send_email(subject=None, body=None):
+def send_email(subject: str = None, body: str = None):
+    """
+
+    :param subject: Text string which will represent subject of a mail
+    :param body: Text string which will represent main text of a mail
+    :return: None
+    """
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
         smtp.ehlo()  # identify us with mail server we are using
         smtp.starttls()  # encrypt our traffic
         smtp.ehlo()  # identify us again as an encrypted connection
 
-        smtp.login(ADDRESS, "".join([chr(int(int_ * 1000)) for int_ in PASS]))
-
+        smtp.login(ADDRESS, PASS)
         msg = f"Subject: {subject}\n\n{body}"  # weird syntax...has to really be like this by default
         smtp.sendmail(from_addr=ADDRESS, to_addrs=ADDRESS, msg=msg)
 
 
-
+send_email()
