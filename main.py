0f2478332ccd04e50bcf83c84d7b449013e9b2c1
tomlazen <oismarilo@gmail.com>
2022-09-07 22:11:04 +0200

    added checking of status & refactored

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-import data_cleaning
+import os
 import scraper
 import plotting
+import data_cleaning
+import constants as c
 
 
-def main(scrape_sitemaps=True,
-         scrape_features=True,
-         clean_features=True,
-         plot_data=True):
-
-    if scrape_sitemaps:
-        scraper.site_map_scraping(scraper.chrome_driver(headless=False))
+def main():
+    if not os.listdir(c.LINKS):
+        scraper.site_map_scraping()
         data_cleaning.delete_unwanted_sitemap_files()
         data_cleaning.clean_raw_data()
 
-    if scrape_features:
-        scraper.features_scraping(driver=scraper.chrome_driver(headless=True),
-                                  batch_size=1_000,
-                                  max_nr_samples=None)
-
-    if clean_features:
-        data_cleaning.concatenate_batches()
-        data_cleaning.clean_feature_data()
-        data_cleaning.aggregate_feature_data()
-        data_cleaning.archive_datasets()
-        data_cleaning.delete_all_batches()
-
-    if plot_data:
-        plotting.plot_all()
+    scraper.features_scraping(batch_size=1000,
+                              max_nr_samples=None,
+                              failed_request_limit=20)
+    data_cleaning.concatenate_batches()
+    data_cleaning.clean_feature_data()
+    data_cleaning.aggregate_feature_data()
+    data_cleaning.archive_datasets()
+    data_cleaning.delete_unwanted_property_files()
+    plotting.plot_all()
 
 
 if __name__ == '__main__':
-    main(scrape_sitemaps=True,
-         scrape_features=False,
-         clean_features=False,
-         plot_data=False)
+    main()
