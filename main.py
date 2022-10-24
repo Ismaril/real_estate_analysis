59d80ec21b34ad0438a12620cdbcd87acee09467
tomlazen <oismarilo@gmail.com>
2022-10-24 18:58:45 +0200

    refactoring

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ import constants as c
 def main():
     if not os.listdir(c.LINKS):
         scraper.site_map_scraping()
-        data_cleaning.delete_unwanted_sitemap_files()
+        data_cleaning.delete_sitemap_files()
         data_cleaning.clean_raw_data()
 
     scraper.features_scraping(batch_size=1000,
                               max_nr_samples=None,
                               failed_request_limit=20)
-    data_cleaning.concatenate_batches()
     data_cleaning.clean_feature_data()
     data_cleaning.aggregate_feature_data()
-    data_cleaning.archive_datasets()
-    data_cleaning.delete_unwanted_property_files()
+    data_cleaning.archive_dataset()
+    data_cleaning.delete_data()
     plotting.plot_all()
 
 
